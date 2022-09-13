7321610cbbaf0193b2f763e0fbfb481e0c97cf16
Nicholaos Bilalis <n.bilalis@sae.edu>
2022-09-13 14:23:01 +0300

    Update `main.py`

--- main.py
+++ main.py
@@ -1,95 +1,84 @@
-import json
+from os import environ
 
-from fastapi import FastAPI, status, HTTPException
+from fastapi import FastAPI, status, Body, HTTPException
+from fastapi.responses import Response, JSONResponse
+from fastapi.encoders import jsonable_encoder
 
-from pydantic import BaseModel
-from typing import Optional
+from typing import List
+from models import Book
 
-file_name = 'books.json'
-
-
-class Book(BaseModel):
-    id: int
-    isbn: str
-    title: str
-    author: Optional[str] = None
-    price: float = None
-
+from motor.motor_asyncio import AsyncIOMotorClient
 
 app = FastAPI()
 
 
-@app.get('/books/')
-async def get_books():
-    with open(file_name) as f:
-        books = json.load(f)
+@app.on_event("startup")
+async def startup_db_client():
+    app.mongodb_client = AsyncIOMotorClient(environ.get('MONGO_URL'))
+    app.db = app.mongodb_client[environ.get('MONGO_DB_NAME')]
 
+
+@app.on_event("shutdown")
+async def shutdown_db_client():
+    app.mongodb_client.close()
+
+
+@app.get('/', tags=['Root'])
+def index():
+    return JSONResponse({'message': 'Welcome to our Bookstore!'})
+
+
+@app.get('/books/', tags=['Books'], response_description="List all books", response_model=List[Book])
+async def get_books():
+    books = await app.db["books"].find().to_list(100)
     return books
 
 
-@app.get('/books/{id}')
-async def get_book(id: int):
-    with open(file_name) as f:
-        books = json.load(f)
+@app.get('/books/{isbn}', tags=['Books'], response_description='Get a single book', response_model=Book)
+async def get_book(isbn: str):
+    if (book := await app.db["books"].find_one({"isbn": isbn})) is not None:
+        return book
 
-    for book in books:
-        if book['id'] == id:
-            return book
-    else:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail="Resource not found")
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Book with ISBN '{isbn}' was not found")
 
 
-@app.post('/books/', status_code=status.HTTP_201_CREATED)
-async def store_book(book: Book):
-    with open(file_name) as f:
-        books = json.load(f)
+@app.post('/books/', tags=['Books'], response_description='Add new student', response_model=Book, status_code=status.HTTP_201_CREATED)
+async def create_book(book: Book = Body(...)):
+    isbn = book.isbn
+    book = jsonable_encoder(book)
 
-    if book.id in [book['id'] for book in books]:
-        raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="Resource already exists")
+    if (existing := await app.db["books"].find_one({"isbn": isbn})) is not None:
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail=f"Book with ISBN '{isbn}' already exists")
 
-    books.append(book.dict())
+    new = await app.db["books"].insert_one(book)
+    created = await app.db["books"].find_one({"_id": new.inserted_id})
 
-    with open(file_name, 'w') as f:
-        json.dump(books, f)
-
-    return book
+    return JSONResponse(status_code=status.HTTP_201_CREATED, content=created)
 
 
-@app.put('/books/')
-async def update_book(new: Book):
-    with open(file_name) as f:
-        books = json.load(f)
+@app.put("/books/", tags=['Books'], response_description="Update a book", response_model=Book)
+async def update_book(book: Book = Body(...)):
+    isbn = book.isbn
+    book = {k: v for k, v in book.dict().items() if k != 'id' and v is not None}
 
-    for book in books:
-        if book['id'] == new.id:
-            book['isbn'] = new.isbn
-            book['title'] = new.title
-            book['author'] = new.author
-            book['price'] = new.price
-            break
-    else:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail="Resource not found")
+    if len(book) >= 1:
+        result = await app.db["books"].update_one({"isbn": isbn}, {"$set": book})
 
-    with open(file_name, 'w') as f:
-        json.dump(books, f)
+        if result.modified_count == 1:
+            if (updated := await app.db["books"].find_one({"isbn": isbn})) is not None:
+                return updated
 
-    return book
+    if (existing := await app.db["books"].find_one({"isbn": isbn})) is not None:
+        return existing
+
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Book with ISBN '{isbn}' was not found")
 
 
-@app.delete('/books/{id}')
-async def remove_book(id: int):
-    with open(file_name) as f:
-        books = json.load(f)
+@app.delete("/books/{isbn}", tags=['Books'], response_description="Delete a book")
+async def delete_book(isbn: str):
+    result = await app.db["books"].delete_one({"isbn": isbn})
 
-    for book in books:
-        if book['id'] == id:
-            books.remove(book)
-            break
-    else:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail="Resource not found")
+    if result.deleted_count == 1:
+        return Response(status_code=status.HTTP_204_NO_CONTENT)
 
-    with open(file_name, 'w') as f:
-        json.dump(books, f)
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Book with ISBN '{isbn}' was not found")
